26d11a7d90e1d2cdd0663ca85e6bd3d94de2c9d0
desperadko <drakostvbg@gmail.com>
2025-10-05 18:28:07 +0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <header>
+
+      </header>
+      <body>
+
+      </body>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
